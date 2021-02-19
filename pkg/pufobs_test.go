3feeb143cbc5bd14718b2790ca5e20025ca9413e
Jonas Levin Weber <jonaslevinweber+git@gmail.com>
2021-02-19 19:37:37 +0100

    Add more tests

--- pkg/pufobs_test.go
+++ pkg/pufobs_test.go
@@ -9,9 +9,25 @@ var textExpectedTemplate = "expected \"%s\" got \"%s\""
 func TestGetFeed(t *testing.T) {
 	feed := GetFeed()
 	if feed == nil {
-		t.Fatalf("feed is nil")
+		t.Fatalf(textExpectedTemplate, "feed", "nil")
 	}
 	if feed.Title != "DAS PODCAST UFO" {
 		t.Fatalf(textExpectedTemplate, "DAS PODCAST UFO", feed.Title)
+	}
+}
+
+func TestGetEpisodes(t *testing.T) {
+	feed := GetFeed()
+	episodes := GetEpisodes()
+	if episodes == nil {
+		t.Fatalf(textExpectedTemplate, "episodes", "nil")
+	}
+	if len(episodes) != len(feed.Items) {
+		t.Fatalf("episodes got %d elements but feed got %d", len(episodes), len(feed.Items))
+	}
+	for _, episode := range episodes {
+		if episode == nil {
+			t.Fatalf("one episode is nil")
+		}
 	}
 }
